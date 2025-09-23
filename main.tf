8ccc9fd039653425e498f49286c3d855c24f6aae
Aiden Cullo <aidencullo@gmail.com>
2025-09-23 10:11:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo 'Hello, World!'"
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
+}
+
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "${path.module}/hello.txt"
+  content  = "Hello, Terraform!"
 }
