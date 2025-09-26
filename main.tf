44b0a756fd1b7cb634abee44669dcf13326f1c77
Aiden Cullo <aidencullo@gmail.com>
2025-09-26 16:44:25 -0400

    Simplify Terraform config - add simple S3 file upload

--- main.tf
+++ main.tf
@@ -12,5 +12,12 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-unique-bucket-12345"
+  bucket = "my-other-other-bucket"
+}
+
+# Upload a simple file to S3
+resource "aws_s3_object" "file_upload" {
+  bucket  = aws_s3_bucket.example.bucket
+  key     = "hello.txt"
+  content = "Hello, World!"
 }
