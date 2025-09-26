33e0520a1d6aaaa302db8850cc9ad794d4f5e447
Aiden Cullo <aidencullo@gmail.com>
2025-09-26 18:19:25 -0400

    Update to reference existing S3 bucket and add goodbye.txt

--- main.tf
+++ main.tf
@@ -11,13 +11,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-other-other-bucket"
-}
-
-# Upload a simple file to S3
+# Upload a file to existing S3 bucket
 resource "aws_s3_object" "file_upload" {
-  bucket  = aws_s3_bucket.example.bucket
-  key     = "hello.txt"
-  content = "Hello, World!"
+  bucket  = "my-other-other-bucket"
+  key     = "goodbye.txt"
+  content = "Goodbye, World!"
+
 }
