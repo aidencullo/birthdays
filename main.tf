7f8b6795061a551915059dad6198cf874c726be2
Aiden Cullo <aidencullo@gmail.com>
2025-10-01 12:30:32 -0400

    still getting │ An argument named "environment" is not expected here.

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
 module "watereye_web" {
   source        = "./modules/ec2-instance"
   environment   = var.environment
-  instance_type = var.instance_type
-  ami_id        = var.ami_id
-  vpc_id        = var.vpc_id
-  subnet_id     = var.subnet_id
-  iam_role_name = var.iam_role_name
-  user_data = templatefile("${path.module}/../../src/user-data.sh", {
-    GITLAB_TOKEN = var.gitlab_token,
-    ARTIFACT_URL = var.artifact_url
-    }
-  )
 }
 
