169407631f4e1bcefcdb41015958c0afbf08f449
Aiden Cullo <aidencullo@gmail.com>
2025-10-28 16:59:09 -0400

    Add user_data to run hello world on EC2 instance

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 resource "aws_instance" "example" {
   ami           = "ami-069f9cce803c015bc"
   instance_type = "t3.micro"
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World" > /tmp/hello.txt
+    cat /tmp/hello.txt
+  EOF
+  
+  tags = {
+    Name = "HelloWorld-Instance"
+  }
 }
