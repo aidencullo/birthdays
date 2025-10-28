f98f68458f7d4e0fe81c92ee18ed81bf77cb56ab
Aiden Cullo <aidencullo@gmail.com>
2025-10-28 18:22:59 -0400

    Add Terraform outputs for instance public IP and ID

--- main.tf
+++ main.tf
@@ -12,3 +12,14 @@ resource "aws_instance" "example" {
     Name = "HelloWorld-Instance"
   }
 }
+
+# Output to display instance information
+output "instance_public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "Public IP address of the EC2 instance"
+}
+
+output "instance_id" {
+  value       = aws_instance.example.id
+  description = "ID of the EC2 instance"
+}
