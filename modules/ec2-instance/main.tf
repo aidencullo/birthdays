6e462300cedb422b059701464f6bce3631944f4e
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 15:58:51 -0400

    auto-pushed changes

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -1,32 +1,6 @@
 # Security group to allow HTTP traffic
 resource "aws_security_group" "web_server" {
   name        = "${var.environment}-web-server"
-  description = "Security group for ${var.environment} web server"
-
-  dynamic "ingress" {
-    for_each = var.allowed_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    {
-      Name        = "${var.environment}-web-server-sg"
-      Environment = var.environment
-    },
-    var.tags
-  )
 }
 
 resource "aws_instance" "web_server" {
