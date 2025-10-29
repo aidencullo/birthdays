3fd86bc3c08b3c5ff39f8f4b8bc4d4710e50bb3e
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 19:23:14 -0400

    auto-pushed changes

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_security_group" "web_server_sg" {
   name        = "web-server-sg"
   description = "Allow SSH and HTTP traffic"
+  vpc_id      = aws_vpc.my_vpc.id
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
 }
