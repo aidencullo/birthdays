7a090abd2b0b2436ccbd81d8050d9926b28f4251
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 17:28:20 -0400

    Update main.tf: set region us-east-1, add SG and EC2 instance

--- main.tf
+++ main.tf
@@ -5,35 +5,10 @@ provider "aws" {
 resource "aws_security_group" "web_server_sg" {
   name        = "web-server-sg"
   description = "Allow SSH and HTTP traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
-
-  tags = {
-    Name = "web-server-instance"
-  }
 }
