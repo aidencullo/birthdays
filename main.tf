f49d241899f4728d74fcfd7943d531cc3f0451ff
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 21:15:27 -0400

    feat(terraform): add SG rules and set static AMI; cleanup

--- main.tf
+++ main.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
@@ -31,11 +16,32 @@ resource "aws_security_group" "web_server_sg" {
   name        = "web-server-sg"
   description = "Allow SSH and HTTP traffic"
   vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_subnet.id
+  ami                    = "ami-06ca3ca175f37dd66"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
 }
