f97355386a904ae7cf7dd224f6c3b52de37bd6b8
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 21:19:42 -0400

    chore(terraform): set subnet AZ to us-west-2a

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-2a"
 }
 
 resource "aws_security_group" "web_server_sg" {
