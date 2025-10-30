8da9d24a666ec9192307b5dc56b937db9e227495
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 21:17:49 -0400

    chore(terraform): switch to us-west-2 and resolve latest AL2 AMI

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -40,7 +55,7 @@ resource "aws_security_group" "web_server_sg" {
 }
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-06ca3ca175f37dd66"
+  ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
