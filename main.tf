385dd03ac977f2d02a796b71b0c2f815c32732ea
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 20:21:33 -0400

    chore(terraform): simplify config by removing defaults and inline region

--- main.tf
+++ main.tf
@@ -1,20 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.5.0"
-}
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "my_vpc" {
