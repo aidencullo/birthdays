ed0faa4c0ea266bad2dfca90097ac9abd2a54ef6
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 18:00:12 -0400

    feat: add security group attachment to EC2 instance

--- main.tf
+++ main.tf
@@ -3,35 +3,35 @@ data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
 }
 
-# resource "aws_security_group" "allow_all" {
-#   name        = "allow_all"
-#   description = "Allow ICMP ping"
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ICMP ping"
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# resource "aws_instance" "web_server" {
-#   ami                    = data.aws_ami.amazon_linux.id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [aws_security_group.allow_all.id]
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 data "aws_instance" "web_server" {
   provider    = aws.east
   instance_id = "i-0eada5807efb1c575"
 }
+
+resource "aws_network_interface_sg_attachment" "attach_allow_all" {
+  security_group_id    = aws_security_group.allow_all.id
+  network_interface_id = data.aws_instance.web_server.network_interface_id
+}
+
 
 
 output "ec2_ip" {
