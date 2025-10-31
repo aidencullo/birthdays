c6772f991f88e5b1c2b175cee96fb26e5a7aeb24
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 14:07:31 -0400

    feat: update AMI to use latest Amazon Linux 2 via data source

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0dc8f589abe99f538"
-  instance_type          = "t3.micro"
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
 }
 
 output "ec2_ip" {
