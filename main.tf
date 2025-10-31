f46168291a51bac93db77fd33d077a5c7a7f3d0e
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 14:19:41 -0400

    chore: push latest changes

--- main.tf
+++ main.tf
@@ -3,10 +3,31 @@ data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
 }
 
-resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.micro"
+resource "aws_security_group" "allow_ping" {
+  name        = "allow_ping"
+  description = "Allow ICMP ping"
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_instance" "web_server" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ping.id]
+}
+
 
 output "ec2_ip" {
   value = aws_instance.web_server.public_ip
