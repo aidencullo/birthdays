25dd58d44064e9c6acd7f2f10ef57ea16acb2b75
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 13:35:03 -0400

    feat: add EC2 IP output

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "aws_instance" "web_server" {
   ami                    = "ami-0dc8f589abe99f538"
   instance_type          = "t3.micro"
 }
+
+output "ec2_ip" {
+  value = aws_instance.web_server.public_ip
+}
