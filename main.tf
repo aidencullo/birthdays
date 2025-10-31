60f84490be30447a7d5e94833715e079eaffa07c
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 13:28:44 -0400

    chore: update terraform config

--- main.tf
+++ main.tf
@@ -1,44 +1,4 @@
-module "vpc" {
-  source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_security_group" "web_server_sg" {
-  name        = "web-server-sg"
-  description = "Allow SSH and HTTP traffic"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "web_server" {
   ami                    = "ami-0dc8f589abe99f538"
   instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.my_subnet.id
-  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
 }
