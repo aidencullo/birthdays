7a53057724edcd1eabc0262920cebbc8595b616e
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 16:48:01 -0400

    feat: configure to use existing EC2 instance

--- main.tf
+++ main.tf
@@ -3,36 +3,37 @@ data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow ICMP ping"
+# resource "aws_security_group" "allow_all" {
+#   name        = "allow_all"
+#   description = "Allow ICMP ping"
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "web_server" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
-}
-
-# data "aws_instance" "existing" {
-#   instance_id = "i-0eada5807efb1c575"
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 # }
+
+# resource "aws_instance" "web_server" {
+#   ami                    = data.aws_ami.amazon_linux.id
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [aws_security_group.allow_all.id]
+# }
+
+data "aws_instance" "web_server" {
+  provider    = aws.east
+  instance_id = "i-0eada5807efb1c575"
+}
 
 
 output "ec2_ip" {
-  value = aws_instance.web_server.public_ip
+  value = data.aws_instance.web_server.public_ip
 }
--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-    region = "eu-west-1"
+    # region = "eu-west-1"
+      region = "us-east-1"
 }
 
 provider "aws" {
