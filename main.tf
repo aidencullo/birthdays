db77de068b11cd85dd935fef3fbeb3d57fc965f1
Aiden Cullo <aidencullo@gmail.com>
2025-11-03 14:47:09 -0500

    refactor: move SG to security_groups.tf and update refs

--- main.tf
+++ main.tf
@@ -3,25 +3,6 @@
 #   owners      = ["amazon"]
 # }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow ICMP ping"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 data "aws_instance" "web_server" {
   provider    = aws.east
   instance_id = "i-0eada5807efb1c575"
